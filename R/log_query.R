e28bb64badc802b4a2e2887380265ddeed5a57dd
Harrison McGehee <harrison.mcgehee@unos.org>
2020-03-05 08:56:55 -0500

    Initial commit

---
+++ R/log_query.R
@@ -0,0 +1,19 @@
+
+# usethis::use_package("DBI")
+# usethis::use_package("logger")
+
+#' get query
+#'
+#' @param conn  connection
+#' @param query query
+#'
+#' @return data.frame
+#' @export
+#'
+log_query <- function(conn, query) {
+    logger::log_info("about to run query ",query)
+    res <- DBI::dbGetQuery(conn, query)
+    logger::log_info("Ran query ", query, " with {nrow(r)} rows returned")
+
+    res
+}
